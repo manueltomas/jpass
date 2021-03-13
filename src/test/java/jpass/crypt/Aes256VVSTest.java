98b35c23651fa5292d492f5f3a4c593608b860c9
Tiagofvarela <Tiagofvarela@users.noreply.github.com>
2021-03-13 18:47:16 +0000

    Finished Crypt tests.

--- src/test/java/jpass/crypt/Aes256VVSTest.java
+++ src/test/java/jpass/crypt/Aes256VVSTest.java
@@ -19,7 +19,7 @@ import org.junit.Test;
 public class Aes256VVSTest {
 	
     @Test(expected = IndexOutOfBoundsException.class)
-    public void shouldEncryptLength16MinusOneInBlockTest() {
+    public void shouldNotEncryptLength16MinusOneInBlockTest() {
         byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
             (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
             (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
@@ -28,13 +28,13 @@ public class Aes256VVSTest {
 
         Aes256 cipher = new Aes256(key);
 
-        byte[] block = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
                 (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee};
-        byte[] encrypted = new byte[16];
-        cipher.encrypt(block, 0, encrypted, 0);
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, 0, outBlock, 0);
     }
 	
-    @Test//(expected = IndexOutOfBoundsException.class)
+    @Test
     public void shouldEncryptLength16PlusOneInBlockTest() {
         byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
             (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
@@ -44,20 +44,42 @@ public class Aes256VVSTest {
 
         Aes256 cipher = new Aes256(key);
 
-        byte[] block = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
                 (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee,
                 (byte) 0xff, (byte) 0x00};
-        byte[] encrypted = new byte[16];
-        cipher.encrypt(block, 0, encrypted, 0);
-        byte[] expectedEncrypted = {(byte) 0x8e, (byte) 0xa2, (byte) 0xb7, (byte) 0xca, (byte) 0x51, (byte) 0x67,
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, 0, outBlock, 0);
+        byte[] expectedOutBlock = {(byte) 0x8e, (byte) 0xa2, (byte) 0xb7, (byte) 0xca, (byte) 0x51, (byte) 0x67,
                 (byte) 0x45, (byte) 0xbf, (byte) 0xea, (byte) 0xfc, (byte) 0x49, (byte) 0x90, (byte) 0x4b, (byte) 0x49,
                 (byte) 0x60, (byte) 0x89};
 
-        Assert.assertTrue(Arrays.equals(expectedEncrypted, encrypted));
+        Assert.assertTrue(Arrays.equals(expectedOutBlock, outBlock));
+    }
+	
+    @Test
+    public void shouldEncryptLength16InBlockTest() {
+        byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
+            (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
+            (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
+            (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1a, (byte) 0x1b, (byte) 0x1c, (byte) 0x1d, (byte) 0x1e,
+            (byte) 0x1f};
+
+        Aes256 cipher = new Aes256(key);
+
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+                (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee,
+                (byte) 0xff};
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, 0, outBlock, 0);
+        byte[] expectedOutBlock = {(byte) 0x8e, (byte) 0xa2, (byte) 0xb7, (byte) 0xca, (byte) 0x51, (byte) 0x67,
+                (byte) 0x45, (byte) 0xbf, (byte) 0xea, (byte) 0xfc, (byte) 0x49, (byte) 0x90, (byte) 0x4b, (byte) 0x49,
+                (byte) 0x60, (byte) 0x89};
+
+        Assert.assertTrue(Arrays.equals(expectedOutBlock, outBlock));
     }
 	
     @Test(expected = IndexOutOfBoundsException.class)
-    public void shouldEncryptLengthZeroOutBlockTest() {
+    public void shouldNotEncryptOutBlockTooSmallTest() {
         byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
             (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
             (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
@@ -66,13 +88,14 @@ public class Aes256VVSTest {
 
         Aes256 cipher = new Aes256(key);
 
-        byte[] block = {(byte) 0x00};
-        byte[] encrypted = new byte[0];
-        cipher.encrypt(block, 0, encrypted, 0);
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+                (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee};
+        byte[] outBlock = new byte[15];
+        cipher.encrypt(inBlock, 0, outBlock, 0);
     }
 	
     @Test
-    public void shouldEncryptLengthOneInBlockTest() {
+    public void shouldEncryptOutBlockSameSizeAsInBlockTest() {
         byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
             (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
             (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
@@ -81,42 +104,20 @@ public class Aes256VVSTest {
 
         Aes256 cipher = new Aes256(key);
 
-        byte[] block = {(byte) 0x00};
-        byte[] encrypted = new byte[16];
-        cipher.encrypt(block, 0, encrypted, 0);
-        
-        byte[] expectedEncrypted = new byte[16];
-        expectedEncrypted[0] = (byte) 0x8e;
-
-        //Assert.assertTrue(Arrays.equals(expectedEncrypted, encrypted));
-        assertEquals(expectedEncrypted, encrypted);
-    }
-	
-    @Test
-    public void shouldEncryptLengthOneOutBlockTest() {
-        byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
-            (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
-            (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
-            (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1a, (byte) 0x1b, (byte) 0x1c, (byte) 0x1d, (byte) 0x1e,
-            (byte) 0x1f};
-
-        Aes256 cipher = new Aes256(key);
-
-        byte[] block = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
                 (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee,
                 (byte) 0xff};
-        byte[] encrypted = new byte[1];
-        cipher.encrypt(block, 0, encrypted, 0);
-        
-        byte[] expectedEncrypted = new byte[1];
-        expectedEncrypted[0] = (byte) 0x8e;
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, 0, outBlock, 0);
+        byte[] expectedOutBlock = {(byte) 0x8e, (byte) 0xa2, (byte) 0xb7, (byte) 0xca, (byte) 0x51, (byte) 0x67,
+                (byte) 0x45, (byte) 0xbf, (byte) 0xea, (byte) 0xfc, (byte) 0x49, (byte) 0x90, (byte) 0x4b, (byte) 0x49,
+                (byte) 0x60, (byte) 0x89};
 
-        //Assert.assertTrue(Arrays.equals(expectedEncrypted, encrypted));
-        assertEquals(expectedEncrypted, encrypted);
+        Assert.assertTrue(Arrays.equals(expectedOutBlock, outBlock));
     }
-    
-    @Test
-    public void shouldEncryptLengthZerdfdsfsdTest() {
+	
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldNotEncryptNegativeInIndexTest() {
         byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
             (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
             (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
@@ -125,17 +126,69 @@ public class Aes256VVSTest {
 
         Aes256 cipher = new Aes256(key);
 
-        byte[] block = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
-            (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee,
-            (byte) 0xff};
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+                (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee};
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, -1, outBlock, 0);
+    }
+	
+    @Test
+    public void shouldEncryptInIndex0Test() {
+        byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
+            (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
+            (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
+            (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1a, (byte) 0x1b, (byte) 0x1c, (byte) 0x1d, (byte) 0x1e,
+            (byte) 0x1f};
 
-        byte[] encrypted = new byte[16];
-        cipher.encrypt(block, 0, encrypted, 0);
-        byte[] expectedEncrypted = {(byte) 0x8e, (byte) 0xa2, (byte) 0xb7, (byte) 0xca, (byte) 0x51, (byte) 0x67,
-            (byte) 0x45, (byte) 0xbf, (byte) 0xea, (byte) 0xfc, (byte) 0x49, (byte) 0x90, (byte) 0x4b, (byte) 0x49,
-            (byte) 0x60, (byte) 0x89};
+        Aes256 cipher = new Aes256(key);
 
-        Assert.assertTrue(Arrays.equals(expectedEncrypted, encrypted));
-        assertEquals(expectedEncrypted, encrypted);
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+                (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee,
+                (byte) 0xff};
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, 0, outBlock, 0);
+        byte[] expectedOutBlock = {(byte) 0x8e, (byte) 0xa2, (byte) 0xb7, (byte) 0xca, (byte) 0x51, (byte) 0x67,
+                (byte) 0x45, (byte) 0xbf, (byte) 0xea, (byte) 0xfc, (byte) 0x49, (byte) 0x90, (byte) 0x4b, (byte) 0x49,
+                (byte) 0x60, (byte) 0x89};
+
+        Assert.assertTrue(Arrays.equals(expectedOutBlock, outBlock));
+    }
+	
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldNotEncryptNegativeOutIndexTest() {
+        byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
+            (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
+            (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
+            (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1a, (byte) 0x1b, (byte) 0x1c, (byte) 0x1d, (byte) 0x1e,
+            (byte) 0x1f};
+
+        Aes256 cipher = new Aes256(key);
+
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+                (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee};
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, 0, outBlock, -1);
+    }
+	
+    @Test
+    public void shouldEncryptOutIndex0Test() {
+        byte[] key = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
+            (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e,
+            (byte) 0x0f, (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14, (byte) 0x15, (byte) 0x16,
+            (byte) 0x17, (byte) 0x18, (byte) 0x19, (byte) 0x1a, (byte) 0x1b, (byte) 0x1c, (byte) 0x1d, (byte) 0x1e,
+            (byte) 0x1f};
+
+        Aes256 cipher = new Aes256(key);
+
+        byte[] inBlock = {(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66,
+                (byte) 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xaa, (byte) 0xbb, (byte) 0xcc, (byte) 0xdd, (byte) 0xee,
+                (byte) 0xff};
+        byte[] outBlock = new byte[16];
+        cipher.encrypt(inBlock, 0, outBlock, 0);
+        byte[] expectedOutBlock = {(byte) 0x8e, (byte) 0xa2, (byte) 0xb7, (byte) 0xca, (byte) 0x51, (byte) 0x67,
+                (byte) 0x45, (byte) 0xbf, (byte) 0xea, (byte) 0xfc, (byte) 0x49, (byte) 0x90, (byte) 0x4b, (byte) 0x49,
+                (byte) 0x60, (byte) 0x89};
+
+        Assert.assertTrue(Arrays.equals(expectedOutBlock, outBlock));
     }
 }
