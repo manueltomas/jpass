c9d5ffbb062d033657b348e740ee3acf801195e8
Tiagofvarela <Tiagofvarela@users.noreply.github.com>
2021-03-27 13:21:00 +0000

    Implemented Configuration test.

--- src/test/java/jpass/util/ConfigurationTest.java
+++ src/test/java/jpass/util/ConfigurationTest.java
@@ -1,20 +1,31 @@
 package jpass.util;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class ConfigurationTest {
 
 	@Test
-	public void shouldStripLengthZeroTest() {
-		String in = "";
+	public void ConfigurationGetDefaultValuesTest() {
+		Configuration cfg = Configuration.getInstance();
+		String key = "not_a_key";
 		
-		String result = StringUtils.stripNonValidXMLCharacters(in);
-		String expected = "";
+		//Test String
+		String defaultString = "s";
+		assertEquals(defaultString, cfg.get(key, defaultString));
 		
-		assertEquals(expected, result);
+		//Test Array
+		String[] defaultArray = {"s"};
+		assertEquals(defaultArray[0], cfg.getArray(key, defaultArray)[0]);
+		assertEquals(1, defaultArray.length);
 		
+		//Test Integer
+		Integer defaultInteger = 0;
+		assertEquals(defaultInteger, cfg.getInteger(key, defaultInteger));
+		
+		//Test Boolean
+		Boolean defaultBoolean = false;
+		assertEquals(defaultBoolean, cfg.is(key, defaultBoolean));
 	}
 }
